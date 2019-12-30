e5c6eeb4782ad5ac15aebd94f31fa3af5a02424b
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-12-30 19:19:00 -0300

    Agora vai

--- src/server.js
+++ src/server.js
@@ -1,19 +1,66 @@
 const fs = require('fs');
+const updater = require('./updater');
 const express = require('express');
 const app = express();
 
-const PORT = '9002';
+const PORT = '9001';
 
-// app.use(express.static('public'));
- 
-app.get('/cota.csv', function (req, res) {
-    fs.readFile('cota.csv', (err, data) => {
-        console.log(data.toString());
-        res.type('text/plain; charset=utf-8');
-        res.send(data.toString());
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+var updating = false;
+var taxa = 0;
+
+app.get('/update', function(req, res){
+    if(!updating){
+        updating = true;
+        taxa = 0;
+        updater(0, function(status){
+            taxa = status;
+            if(taxa >= 1){
+                backupCopy('open.csv');
+                backupCopy('high.csv');
+                backupCopy('low.csv');
+                backupCopy('close.csv');
+                backupCopy('adj.csv');
+                backupCopy('vol.csv');
+                backupCopy('div.csv');
+                backupCopy('split.csv');
+                updating = false;
+            }
+        });
+    }
+    res.redirect('/status');
+});
+
+app.get('/status', function(req, res){
+    res.json({
+        updating: updating,
+        taxa: taxa,
+        lastUpdate: 'TODO'
     });
 });
 
 app.listen(PORT);
 
-module.exports = app;
+async function backupCopy(fileName){
+    try{
+        fs.copyFileSync('files/backup/' + fileName + '.bkp1', 'files/backup/' + fileName + '.bkp2');
+    }
+    catch(e){}
+    finally{
+        try{
+            fs.copyFileSync('files/' + fileName, 'files/backup/' + fileName + '.bkp1');
+        }
+        catch(e){}
+        finally{
+            try{
+                fs.copyFileSync('tmpFiles/' + fileName, 'files/' + fileName);
+            }
+            catch(e){
+                console.log("Erro ao copiar arquivo " + fileName);
+                console.log(e);
+            }
+        }
+    }
+}
---
+++ src/updater.js
@@ -0,0 +1,159 @@
+const fs = require('fs');
+const alpha = require('alphavantage')({key: process.env.AV_KEY});
+const symbols = ['^BVSP', 'ALPA4', 'AMAR3', 'ARZZ3', 'BBAS3', 'BBDC3'];
+//, 'BBDC4', 'BOVA11', 'CAML3', 'CMIG4', 'COGN3', 'CPFE3', 'CPLE6', 'CSMG3', 'CSNA3', 'CYRE3', 'DTEX3', 'EGIE3', 'ENBR3', 'ENEV3', 'ENGI11', 'EQTL3', 'FESA4', 'FLRY3', 'GGBR4', 'GOAU3', 'GOLL4', 'GRND3', 'GUAR3', 'ITSA4', 'ITUB3', 'ITUB4', 'JHSF3', 'LREN3', 'MDIA3', 'MRVE3', 'MULT3', 'ODPV3', 'OFSA3', 'PARD3', 'QUAL3', 'RADL3', 'RENT3', 'SANB11', 'SAPR4', 'SBSP3', 'SEER3', 'SMTO3', 'TEND3', 'TRIS3', 'TRPL4', 'USIM3', 'VIVT4', 'VULC3', 'WEGE3', 'YDUQ3'];
+
+var table = [];
+
+async function pullData(index = 0, callback){
+
+    const startTime = Date.now();
+
+    symbolA = symbols[index];
+    symbolB = symbols[index + 1];
+    symbolC = symbols[index + 2];
+    symbolD = symbols[index + 3];
+    symbolE = symbols[index + 4];
+
+    await setData(symbolA);
+    await setData(symbolB);
+    await setData(symbolC);
+    await setData(symbolD);
+    await setData(symbolE);
+
+    if(index < symbols.length - 5){
+        callback(index/symbols.length);
+        var timeNow = Date.now();
+        while((timeNow - startTime) < 70000){
+            timeNow = Date.now();
+        }
+        pullData(index + 5, callback);
+    }
+    else{
+        newTable = {};
+        Object.keys(table).sort().forEach(key => { // Ordena as datas
+            if(key > '2009-12-31'){ // Retira datas anteriores a 2010
+                newTable[key] = table[key];
+            }
+        });
+        writeCSV(newTable);
+        callback(1);
+    }
+}
+
+async function setData(symbol){
+    if(symbol){
+        try {
+            const data = await alpha.data.daily_adjusted((symbol == '^BVSP')?symbol:(symbol + '.SA'), "full", "json");
+            var hist = data['Time Series (Daily)'];
+            for (var key in hist){
+                if(!table[key]){
+                    table[key] = [];
+                }
+                table[key][symbol] = [];
+                table[key][symbol]['open']  = hist[key]['1. open'].replace('.', ',');
+                table[key][symbol]['high']  = hist[key]['2. high'].replace('.', ',');
+                table[key][symbol]['low']   = hist[key]['3. low'].replace('.', ',');
+                table[key][symbol]['close'] = hist[key]['4. close'].replace('.', ',');
+                table[key][symbol]['adj']   = hist[key]['5. adjusted close'].replace('.', ',');
+                table[key][symbol]['vol']   = hist[key]['6. volume'].replace('.', ',');
+                table[key][symbol]['div']   = hist[key]['7. dividend amount'].replace('.', ',');
+                table[key][symbol]['split'] = hist[key]['8. split coefficient'].replace('.', ',');
+            }
+        }
+        catch(e){
+            console.log('ERRO AO BAIXAR INFORMAÇÕES SOBRE O ATIVO ' + symbol);
+            console.log(e);
+        }
+    }
+}
+
+function writeCSV(table){
+    
+    deleteAll();
+
+    // Escreve o cabeçalho
+    writeAll('Data');
+    symbols.forEach(symbol => {
+        writeAll(';' + symbol);
+    });
+    writeAll('\n');
+
+
+    // Escreve as datas e suas respectivas cotações
+    for(date in table){
+        writeAll(date);
+        symbols.forEach(symbol => {
+            writeAll(';');
+            if(table[date][symbol]){
+                writeAll(table[date][symbol], true);
+            }
+            else{
+                writeAll('FALSO');
+            }
+        });
+        writeAll('\n');
+    }
+}
+
+function deleteAll(){
+    try{
+        fs.unlinkSync('tmpFiles/open.csv');
+    }
+    catch(e){}
+    finally{
+        try{
+            fs.unlinkSync('tmpFiles/high.csv');
+        }
+        catch(e){}
+        finally{
+            try{
+                fs.unlinkSync('tmpFiles/low.csv');
+            }
+            catch(e){}
+            finally{
+                try{
+                    fs.unlinkSync('tmpFiles/close.csv');
+                }
+                catch(e){}
+                finally{
+                    try{
+                        fs.unlinkSync('tmpFiles/adj.csv');
+                    }
+                    catch(e){}
+                    finally{
+                        try{
+                            fs.unlinkSync('tmpFiles/vol.csv');
+                        }
+                        catch(e){}
+                        finally{
+                            try{
+                                fs.unlinkSync('tmpFiles/div.csv');
+                            }
+                            catch(e){}
+                            finally{
+                                try{
+                                    fs.unlinkSync('tmpFiles/split.csv');
+                                }
+                                catch(e){}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function writeAll(content, flag){
+    fs.appendFileSync('tmpFiles/open.csv', flag?(content['open'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/high.csv', flag?(content['high'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/low.csv', flag?(content['low'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/close.csv', flag?(content['close'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/adj.csv', flag?(content['adj'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/vol.csv', flag?(content['vol'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/div.csv', flag?(content['div'] || 'FALSO'):content);
+    fs.appendFileSync('tmpFiles/split.csv', flag?(content['split'] || 'FALSO'):content);
+}
+
+module.exports = pullData;
